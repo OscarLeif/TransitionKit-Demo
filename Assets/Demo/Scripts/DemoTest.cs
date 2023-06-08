2690c289b3d17e7cd82dbd613e6fe76da9dd0208
Oscar Fernandez <oscar.leif@gmail.com>
2023-06-07 21:04:33 -0500

    Rewriting the Demo Plugin

--- Assets/Demo/Scripts/DemoTest.cs
+++ Assets/Demo/Scripts/DemoTest.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 /// Canvas Test Example
 /// </summary>
 public class DemoTest : MonoBehaviour
-{
+{    
     public const string DemoSceneA = "DemoSceneA";
     public const string DemoSceneB = "DemoSceneB";
 
@@ -18,26 +18,25 @@ public class DemoTest : MonoBehaviour
 
     public enum TransitionType { Clasic, Coroutine, Async }
 
-    [SerializeField] public Transform FollowMe;
+    [SerializeField] public Transform FollowMe;    
 
     private void Start()
-    {
-        DontDestroyOnLoad(this);
+    {        
         FadeButton.onClick.AddListener(OnFadeExample);
         OpenCircleButton.onClick.AddListener(OpenCircleScene);
     }
 
     private void OpenCircleScene()
     {
-        if (TransitionKit.IsWorking) return;
+        //if (TransitionKit.IsWorking) return;
 
         string nextScene = GetNextScene();
 
-        TransitionKit.OnTransitionStart += OnTransitionKitStart;
-        TransitionKit.OnTransitionCompleted += OnTransitionKitEnd;
+        //TransitionKit.OnTransitionStart += OnTransitionKitStart;
+        //TransitionKit.OnTransitionCompleted += OnTransitionKitEnd;
 
-        TransitionKit.BeforeSceneLoad += BeforeSceneLoad;
-        TransitionKit.AfterSceneLoad += AfterSceneLoad;
+        //TransitionKit.BeforeSceneLoad += BeforeSceneLoad;
+        //TransitionKit.AfterSceneLoad += AfterSceneLoad;
 
         //FollowTransform
         //TransitionKit.OpenCircle(nextScene, 5f, Color.black, FollowMe);
@@ -51,38 +50,9 @@ public class DemoTest : MonoBehaviour
     /// </summary>
     private void OnFadeExample()
     {
-        if (TransitionKit.IsWorking) return;
-
         string nextScene = GetNextScene();
 
-        TransitionKit.OnTransitionStart += OnTransitionKitStart;
-        TransitionKit.OnTransitionCompleted += OnTransitionKitEnd;
-
-        TransitionKit.BeforeSceneLoad += BeforeSceneLoad;
-        TransitionKit.AfterSceneLoad += AfterSceneLoad;
-
-       
-        switch(transitionType)
-        {
-            //Clasic Way
-            case TransitionType.Clasic:
-                TransitionKit.FadeScene(nextScene, 1f, Color.black);
-                break;
-            case TransitionType.Coroutine://Coroutine Version
-                var fader = new FadeTransition();
-                fader.transitionTime = 1f;
-                fader.fadeToColor = Color.black;
-                fader.nextSceneName = GetNextScene();
-                TransitionKit.Instance.StartCoroutine(TransitionKit.Instance.TransitionWithDelegate(fader));
-                break;
-            case TransitionType.Async://Async Version...Advantage of No isues with Enable Disable GameObject                
-                var faderAsync = new FadeTransition();
-                faderAsync.transitionTime = 1f;
-                faderAsync.fadeToColor = Color.black;
-                faderAsync.nextSceneName = GetNextScene();
-                TransitionKit.Instance.TransitionWithDelegateTask(faderAsync);
-                break;
-        }        
+        TransitionKit.Get.FadeScene(nextScene, 1f, Color.black);             
     }
 
     private void OnTransitionKitStart()
